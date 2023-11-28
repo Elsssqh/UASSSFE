8d1a4c08277f20f80d62be73c2f10513c1279800
Elsssqh <elss.hqh@gmail.com>
2023-11-28 18:49:12 +0700

    ourboooook

---
+++ BookAPI.js
@@ -0,0 +1,41 @@
+// script-angular.js
+// APIKEY : AIzaSyCvJYVNcx7FGTjOkeOqgHNrzKB3Y7F5ulU
+angular.module('bookApp', [])
+    .controller('BookController', function ($scope, $http) {
+        $scope.books = [];
+        $scope.selectedBook = null;
+
+        $http.get('https://www.googleapis.com/books/v1/volumes?q=novel&langRestrict=en&maxResults=30&key=AIzaSyCvJYVNcx7FGTjOkeOqgHNrzKB3Y7F5ulU')
+            .then(function (response) {
+                console.log('API Response:', response.data);
+
+                if (response.data.items) {
+                    $scope.books = response.data.items.map(function (item) {
+                        // Truncate the description to a specific length (e.g., 150 characters)
+                        const truncatedDescription = item.volumeInfo.description ? item.volumeInfo.description.slice(0, 150) + '...' : 'No description available.';
+
+                        return {
+                            title: item.volumeInfo.title || 'No Title',
+                            authors: item.volumeInfo.authors || ['Unknown Author'],
+                            description: truncatedDescription,
+                            thumbnail: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : 'no-image.jpg'
+                        };
+                    });
+                } else {
+                    console.error('Error fetching book data.');
+                }
+            })
+            .catch(function (error) {
+                console.error('Error fetching book data:', error);
+            });
+
+        $scope.showBookDetails = function (book) {
+            $scope.selectedBook = book;
+            document.getElementById('overlay').style.display = 'flex';
+        };
+
+        $scope.hideBookDetails = function () {
+            $scope.selectedBook = null;
+            document.getElementById('overlay').style.display = 'none';
+        };
+    });
---
+++ search.js
@@ -0,0 +1,50 @@
+
+// script-angular.js
+
+angular.module('bookApp', [])
+    .controller('BookController', function ($scope, $http) {
+        $scope.books = [];
+        $scope.selectedBook = null;
+        $scope.searchQuery = '';
+
+        $scope.searchBooks = function () {
+            // Check if the search query is not empty
+            if ($scope.searchQuery.trim() !== '') {
+                $http.get('https://www.googleapis.com/books/v1/volumes?q=' + $scope.searchQuery + '&langRestrict=en&maxResults=30&key=AIzaSyCvJYVNcx7FGTjOkeOqgHNrzKB3Y7F5ulU')
+                    .then(function (response) {
+                        console.log('API Response:', response.data);
+
+                        if (response.data.items) {
+                            $scope.books = response.data.items.map(function (item) {
+                                // Truncate the description to a specific length (e.g., 150 characters)
+                                const truncatedDescription = item.volumeInfo.description ? item.volumeInfo.description.slice(0, 130) + '...' : 'No description available.';
+
+                                return {
+                                    title: item.volumeInfo.title || 'No Title',
+                                    authors: item.volumeInfo.authors || ['Unknown Author'],
+                                    description: truncatedDescription,
+                                    thumbnail: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : 'no-image.jpg'
+                                };
+                            });
+                        } else {
+                            console.error('Error fetching book data.');
+                        }
+                    })
+                    .catch(function (error) {
+                        console.error('Error fetching book data:', error);
+                    });
+            } else {
+                console.warn('Empty search query.');
+            }
+        };
+
+        $scope.showBookDetails = function (book) {
+            $scope.selectedBook = book;
+            document.getElementById('overlay').style.display = 'flex';
+        };
+
+        $scope.hideBookDetails = function () {
+            $scope.selectedBook = null;
+            document.getElementById('overlay').style.display = 'none';
+        };
+    });
